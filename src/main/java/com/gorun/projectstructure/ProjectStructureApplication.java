87549d14defc4f98681c3cc54971120ca035a585
dongtiandexue <huawei_code@163.com>
2020-05-10 20:10:29 +0800

    Initial commit

---
+++ src/main/java/com/gorun/projectstructure/ProjectStructureApplication.java
@@ -0,0 +1,13 @@
+package com.gorun.projectstructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gorun/projectstructure/ProjectStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorun.projectstructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
