c0d5651d16a66265645adb239f3d27dadab4a041
dongtiandexue <huawei_code@163.com>
2020-05-31 10:23:34 +0800

    Ipv4地址的转换工具类

---
+++ src/main/java/com/gorun/projectstructure/exception/GlobalException.java
@@ -0,0 +1,10 @@
+package com.gorun.projectstructure.exception;
+
+/**
+ * @author Running boy
+ * @Description: 全局异常处理
+ * @date 2020/5/25 0:08
+ */
+// @RestControllerAdvice
+public class GlobalException {
+}
---
+++ src/main/java/com/gorun/projectstructure/exception/ServiceException.java
@@ -0,0 +1,9 @@
+package com.gorun.projectstructure.exception;
+
+/**
+ * @author Running boy
+ * @Description: TODO
+ * @date 2020/5/25 0:07
+ */
+public class ServiceException {
+}
